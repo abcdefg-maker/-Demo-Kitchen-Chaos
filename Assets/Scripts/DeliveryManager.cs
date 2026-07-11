d3b43aa57ce54cce997951053e89f09b9fa3fde0
穆奕欣 <m982144738@163.com>
2026-07-12 02:18:33 +0800

    完成菜单生成和提交的同步逻辑
    1. DeliveryManager挂载network object脚本
    2. 生成菜单处 采用rpc同步，没有使用网络变量，因此迟到进入游戏可能会导致玩家之间看到的菜单不同步，这里设计强制玩家同时进入游戏来解决这个问题
    3. 分别包含成功提交和提交失败的逻辑，整体相似，都包括clientRPC和ServerRPC的嵌套，详细逻辑（以提交成功为例子）：
        本地匹配成功 → ServerRpc（上报给服务器，因为客户端无权广播） → ClientRpc（服务器广播给所有人，大家一起删）
    
    另，附diff between ClientServer & ServerRPC：
       server：由客户端调用，（函数里的代码）在服务器上执行
       client：由服务器调用，（函数里的代码）在客户端上执行
    
    常用流程例子：
    客户端按键
      → 调用 ServerRpc（请求服务器处理）
        → 服务器验证并修改状态（权威逻辑）
          → 调用 ClientRpc（把结果告诉所有客户端）
            → 所有客户端播放动画/更新表现

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -2,8 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class DeliveryManager : MonoBehaviour 
+using Unity.Netcode;
+public class DeliveryManager : NetworkBehaviour 
     //存储顾客等待的菜单
 {
     public event EventHandler OnRecipeSpawned;      //产生recipe的时候，控制ui显示的事件
@@ -30,6 +30,13 @@ public class DeliveryManager : MonoBehaviour
     }
     private void Update()
     {
+        //只在server端运行，server端负责产生新的recipeSO，并通过ClientRpc传给所有客户端
+        if (!IsServer)
+        {
+            return;
+        }
+        
+
         //定期产生订单recipe
         spawnRecipeTimer -= Time.deltaTime;
         if(spawnRecipeTimer <= 0f)
@@ -38,14 +45,27 @@ public class DeliveryManager : MonoBehaviour
 
             if (KitchenGameManager.Instance.IsGamePlaying() && waitingRecipeSOList.Count < waitingRecipesMax)
             {
-                RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[UnityEngine.Random.Range(0, recipeListSO.recipeSOList.Count)];
+                int waitingRecipeSOIndex = UnityEngine.Random.Range(0, recipeListSO.recipeSOList.Count);
                 //recipeSOList是RecipeListSO类内的一个list，list类内部存储着所有的recipeSO
-                waitingRecipeSOList.Add(waitingRecipeSO);
-
-                OnRecipeSpawned?.Invoke(this,EventArgs.Empty);
+                SpawnNewWaitingRecipeClientRpc(waitingRecipeSOIndex);
             }
         }
     }
+
+    //这里考虑到游戏性质（和作者偷懒hhh），没有使用网络变量来同步菜单，所以如果玩家进入游戏时间不同，可能会导致菜单不同步，玩家看到的菜单可能不一样
+    //后续通过控制玩家同时进入游戏来解决这个问题
+
+    //只在server端调用，server端负责产生新的recipeSO，并通过ClientRpc传给所有客户端
+    [ClientRpc]//代码在所有客户端上运行，服务器将消息广播给客户端
+    private void SpawnNewWaitingRecipeClientRpc(int waitingRecipeSOIndex)
+    {
+        RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[waitingRecipeSOIndex];
+
+        waitingRecipeSOList.Add(waitingRecipeSO);
+
+        OnRecipeSpawned?.Invoke(this,EventArgs.Empty);
+    }
+
     /// <summary>
     /// 负责检查传送带接收的菜品，检查菜品是否合格
     /// </summary>
@@ -88,18 +108,49 @@ public class DeliveryManager : MonoBehaviour
                 if (plateContentsMatchesRecipe)
                 {
                     //两个recipe的元素个数相同 且 所有元素都匹配成功了
-                    waitingRecipeSOList.RemoveAt(i); //把提交成功的菜品从等待名单中去掉
-                    successfulRecipeAmount++;
-                    OnRecipeCompleted?.Invoke(this,EventArgs.Empty);  
-                    OnRecipeSuccess?.Invoke(this,EventArgs.Empty); //播放成功音效
+                    DeliverCorrectRecipeServerRpc(i);
                     return;
                 }
             }
         }
         //没有匹配成功的选项
         //玩家提交的recipe有问题
-        OnRecipeFailed?.Invoke(this,EventArgs.Empty); //播放失败音效
+        DeliverRecipeIncorrectServerRpc();
         return;
+    }
+
+    //关于下面这段嵌套关系的解释：
+    //本地匹配成功
+    //→ ServerRpc（上报给服务器，因为客户端无权广播）
+    //→ ClientRpc（服务器广播给所有人，大家一起删）
+
+    //提交成功的RPC网络同步逻辑
+    //代码在服务器上运行，客户端调用给服务器同步状态
+    [ServerRpc(RequireOwnership = false)]//RequireOwnership = false的意思是，
+                                         //客户端可以调用ServerRpc函数，而不需要拥有这个对象的所有权
+    public void DeliverCorrectRecipeServerRpc(int waitingRecipeSOIndex) 
+    {
+        DeliverCorrectRecipeClientRpc(waitingRecipeSOIndex);
+    }
+    [ClientRpc]
+    private void DeliverCorrectRecipeClientRpc(int waitingRecipeSOIndex)
+    {
+        waitingRecipeSOList.RemoveAt(waitingRecipeSOIndex); //把提交成功的菜品从等待名单中去掉
+        successfulRecipeAmount++;
+        OnRecipeCompleted?.Invoke(this,EventArgs.Empty);  
+        OnRecipeSuccess?.Invoke(this,EventArgs.Empty); //播放成功音效
+    }
+
+    //提交失败的RPC网络同步逻辑
+    [ServerRpc(RequireOwnership = false)]
+    private void DeliverRecipeIncorrectServerRpc()
+    {
+        DeliverRecipeFailedClientRpc();
+    }
+    [ClientRpc]
+    private void DeliverRecipeFailedClientRpc()
+    {
+        OnRecipeFailed?.Invoke(this,EventArgs.Empty); //播放失败音效
     }
 
     public List<RecipeSO> GetWaitingRecipeSOList()
