25396ffa07450adfb5e9dd8f15422408ff703ac5
穆奕欣 <m982144738@163.com>
2026-07-14 21:25:43 +0800

    解决盘子生成的同步

--- Assets/Scripts/Counters/PlatesCounter.cs
+++ Assets/Scripts/Counters/PlatesCounter.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
 public class PlatesCounter :BaseCounter 
 {
@@ -17,6 +18,11 @@ public class PlatesCounter :BaseCounter
 
     private void Update()
     {
+        if(!IsServer) //只在服务器上生成盘子
+        {
+            return;
+        }
+
         spawnPlateTimer += Time.deltaTime;
         if(spawnPlateTimer > spawnPlateTimerMax)
         {
@@ -24,25 +30,51 @@ public class PlatesCounter :BaseCounter
             
             if(KitchenGameManager.Instance.IsGamePlaying() && plateSpawnedAmount < plateSpawnedAmountMax)
             {
-                plateSpawnedAmount++;
-
-                OnPlateSpawned?.Invoke(this, EventArgs.Empty);
+                SpawnPlateServerRpc();
             }
         }
     }
+
+    [ServerRpc] //只有服务器允许生成盘子，所有这里不写RequireOwnership = false
+    private void SpawnPlateServerRpc()
+    {
+        SpawnPlateClientRpc();
+    }
+
+    [ClientRpc]
+    private void SpawnPlateClientRpc()
+    {
+        plateSpawnedAmount++;
+
+        OnPlateSpawned?.Invoke(this, EventArgs.Empty);
+    }
+
     public override void Interact(Player player)
     {
         if (!player.HasKitchenObject())
         {
             if(plateSpawnedAmount > 0) 
             {
-                //至少有一个盘子
-                plateSpawnedAmount--;
-
                 //玩家把盘子拿起来
                 KitchenObject.SpawnKitchenObject(plateKitchenObjectSO, player);
-                OnPlateRemoved?.Invoke(this, EventArgs.Empty); //减少视觉上盘子数量的事件
+
+                InteractLogicServerRpc();  //触发事件
             }
         }
+    }
+
+
+    [ServerRpc(RequireOwnership = false)]
+    public void InteractLogicServerRpc()
+    {
+        InteractLogicClientRpc();
+    }
+
+    [ClientRpc]
+    public void InteractLogicClientRpc()
+    {
+         //至少有一个盘子
+        plateSpawnedAmount--;
+        OnPlateRemoved?.Invoke(this, EventArgs.Empty); //减少视觉上盘子数量的事件
     }
 }
